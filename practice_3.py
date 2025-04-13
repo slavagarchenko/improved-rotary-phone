26f089fc0f02999d9ea3f90d85563a3552907cd9
Vyacheslav Garchenko <cvgarchenko@gmail.com>
2025-04-13 22:03:33 +0700

    new practice

--- practice_3.py
+++ practice_3.py
@@ -1,43 +1,44 @@
+from datetime import date
 import math
 
 
-#1
+# 1
 btc_rub = float(input())
-print(round((btc_rub//100)%10))
+print(round((btc_rub//100) % 10))
 
 
-#2
-time=int(input())
-hours=time//3600
-min=(time-hours*3600)//60
-sec=time-hours*3600-min*60
+# 2
+time = int(input())
+hours = time//3600
+min = (time-hours*3600)//60
+sec = time-hours*3600-min*60
 print(f'{hours} часов {min} минут {sec} секунд')
 
 
-#3
-number=int(input())
-print(number%2)
+# 3
+number = int(input())
+print(number % 2)
 
 
-#4
+# 4
 data = input()
-x, y, n=map(int, data.split())
+x, y, n = map(int, data.split())
 rub = x*n+(y*n)//100
-kop = (y*n)%100
+kop = (y*n) % 100
 print(f'{rub} рублей {kop} копеек')
 
 
-#5
+# 5
 n = int(input())
-L_bracket=chr(ord('('))
-R_bracket=chr(ord(')'))
-R_slash=chr(92)
-L_slash=chr(ord('/'))
-underline=chr(ord('_'))
-equal=chr(ord('='))
-dot=chr(ord('.'))
-quotes=chr(ord("'"))
-Double_quotes=chr(ord('"'))
+L_bracket = chr(ord('('))
+R_bracket = chr(ord(')'))
+R_slash = chr(92)
+L_slash = chr(ord('/'))
+underline = chr(ord('_'))
+equal = chr(ord('='))
+dot = chr(ord('.'))
+quotes = chr(ord("'"))
+Double_quotes = chr(ord('"'))
 up = L_bracket + R_slash + 3*underline + L_slash + R_bracket + ' '
 middle = L_bracket+equal + quotes + dot + quotes + equal + R_bracket + ' '
 down = (L_bracket + Double_quotes + R_bracket + underline +
@@ -45,7 +46,7 @@ down = (L_bracket + Double_quotes + R_bracket + underline +
 print(f'{up*n}\n{middle*n}\n{down*n}')
 
 
-#6
+# 6
 k = input()
 n = int(input())
 r = int(input())
@@ -53,14 +54,14 @@ sequence = int(k*n)
 print(sequence*r)
 
 
-#7
+# 7
 raw = input('enter number: ')
 num = int(raw, 36)
-prunt(num)
+print(num)
 
-#8
+# 8
 data = input()
-a, b, c=map(int, data.split())
+a, b, c = map(int, data.split())
 
 if a + b > c and a + c > b and b + c > a:
     angle_a = math.acos((b**2 + c**2 - a**2) / (2 * b * c))
@@ -78,12 +79,12 @@ else:
     print("Такой треугольник не существует.")
 
 
-#9
-ATT = int(input()) # попытки прохождения
-COMP = int(input()) # завершения
-YDS = int(input()) # пробежки
-TD = int(input()) # пасы на тачдаун
-INT = int(input()) # перехваты
+# 9
+ATT = int(input())  # попытки прохождения
+COMP = int(input())  # завершения
+YDS = int(input())  # пробежки
+TD = int(input())  # пасы на тачдаун
+INT = int(input())  # перехваты
 a = ((COMP / ATT) - 0.3) * 5
 b = ((YDS / ATT)-3) * 0.25
 c = (TD / ATT) * 20
@@ -92,33 +93,33 @@ passer_rating = (a + b + c + d) / 6 * 100
 print(passer_rating)
 
 
-#10
+# 10
 data = input()
 x, y = map(int, data.split())
 max_number = max(x, y)
 min_number = min(x, y)
-print(max_number%min_number+1)
+print(max_number % min_number+1)
 
 
-#11
+# 11
 n = int(input())
 hours = n//30
 minutes = (n-hours*30)*2
 print(f'{hours} часов {minutes} минут')
 
 
-#12
-from datetime import date
+# 12
 print(date.today())
 
 
-#13
-n = int(input()) #колво строк на странице
-c = int(input()) #колво стобцов на странице
+# 13
+n = int(input())  # колво строк на странице
+c = int(input())  # колво стобцов на странице
 record_number = int(input())
 records_per_page = c*n
 page_number = (record_number-1)//records_per_page+1
-record_on_page = (record_number-1)%records_per_page
+record_on_page = (record_number-1) % records_per_page
 column_number = (record_on_page//n)+1
-string_number = (record_on_page%n)+1
-print(f'страница {page_number}, стобец {column_number}, строка {string_number}')
+string_number = (record_on_page % n)+1
+print(
+    f'страница {page_number}, стобец {column_number}, строка {string_number}')
---
+++ practice_9.py
@@ -0,0 +1,110 @@
+# #1
+# data = input()
+# N,d,r = map(float, data.split(' '))
+# print(2*d+2*r)*N
+
+# # 2
+# number = float(input())
+# num = number
+# while num > 2:
+#     num = number**0.5
+#     number = num
+#     print(round(num, 3))
+
+# # 3
+# ice = int(input())
+# for i in range(215, ice):
+#     if ice % i == 0:
+#         print(i)
+#         break
+
+# # 4
+# count = 0
+# digit = int(input())
+# while digit != 0:
+#     digit = int(input())
+#     if digit % 4 == 0:
+#         count += 1
+# print(count-1)
+
+# # 5
+# for i in range(100000, 999999):
+#     if str(i)[1] == str(i)[5] and str(i)[2] == str(i)[4]:
+#         if str(i+1)[1] == str(i+1)[5] and str(i+1)[2] == str(i+1)[4]:
+#             if str(i+2)[1] == str(i+2)[4] and str(i+2)[2] == str(i+2)[3]:
+#                 if str(i+3)[0] == str(i+3)[5] and str(i+3)[1] == str(i+3)[4] and str(i+3)[2] == str(i+3)[4]:
+#                     print(i)
+
+# #6
+# for i in range(10,100):
+#     if i**2%100 == i:
+#         break
+# print(f' {i}\n*{i}\n___\n\n {i**2}')
+
+# # 7
+# for i in range(10000, 19999):
+#     m = i // 10000
+#     o = i % 10000 // 1000
+#     n = i % 1000 // 100
+#     e = i % 100 // 10
+#     y = i % 10
+
+#     if len({m, o, n, e, y}) != 5:
+#         continue
+
+#     for s in range(1, 10):
+#         for d in range(1, 10):
+#             for r in range(1, 10):
+#                 if (s == d or s == r or d == r or
+#                         s in {m, o, n, e, y} or
+#                         d in {m, o, n, e, y} or
+#                         r in {m, o, n, e, y}):
+#                     continue
+
+#                 send = s * 1000 + e * 100 + n * 10 + d
+#                 more = m * 1000 + o * 100 + r * 10 + e
+
+#                 if send + more == i:
+#                     print(f"SEND = {send}, MORE = {more}, MONEY = {i}")
+
+# # 8
+# x = int(input())
+# count = 0
+# for i in range(1, x-1):
+#     for k in range(1, x-1):
+#         if i**2 + k**2 == x:
+#             count += 1
+# print(round(count/2))
+
+# # 9
+# N = int(input())
+# count = 0
+# for i in range(N+1):
+#     for k in range(N+1):
+#         count += k
+#     count += i
+# print(count)
+
+# # 10
+# n = int(input())
+# count = 0
+# stairs = [0] * n
+# level = 1
+# stairs[0] = n
+
+# while level > 0:
+#     stairs[level - 1] -= 1
+#     remaining = sum(stairs[:level])
+
+#     if stairs[level - 1] <= 0:
+#         level -= 1
+#         continue
+
+#     if remaining == 0:
+#         count += 1
+#         level -= 1
+#     elif remaining > stairs[level - 1]:
+#         stairs[level] = min(stairs[level - 1], remaining)
+#         level += 1
+
+# print(count)

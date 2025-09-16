a3c655dcc7a331ac6e07d8152aa352e34ffe8b53
slavagarchenko <cvgarchenko@gmail.com>
2025-09-16 09:48:51 +0700

    Update num5.py

--- num5.py
+++ num5.py
@@ -2,13 +2,17 @@ a = input()
 b = input()
 c = input()
 s = set()
+
 for i in a:
     if i not in b and i not in c:
         s.add(i)
+        
 for k in b:
     if k not in a and k not in c:
         s.add(k)
+        
 for g in c:
     if g not in b and g not in a:
         s.add(g)
+        
 print(s)

caec2617c9e005bfb69a52a05eebb91076be9a18
slavagarchenko <cvgarchenko@gmail.com>
2025-09-16 09:35:37 +0700

    Create num4.py

---
+++ num4.py
@@ -0,0 +1,13 @@
+import re
+text = input()
+found  = None
+for item in set(text):
+    symbol = re.escape(item)
+    count = len(re.findall(symbol, text))
+    if count == 3:
+        found = item
+        break
+if found is not None:
+    print(found)
+else:
+    print("None")

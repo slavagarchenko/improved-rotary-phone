80b196ed01792c7e3d549f93d69fd358ca894ca7
slavagarchenko <cvgarchenko@gmail.com>
2025-09-16 09:46:45 +0700

    Update num13.py

--- num13.py
+++ num13.py
@@ -1,15 +1,21 @@
 count = 1
+
 while True:
     bilet = input().strip()
     if len(bilet) % 2 == 0:
+        
         first_half_sum = 0
         second_half_sum = 0
         half = len(bilet)//2
+        
         for i in range(half):
             first_half_sum += int(bilet[i])
+            
         for i in range(half, len(bilet)):
             second_half_sum += int(bilet[i])
+            
         if first_half_sum == second_half_sum:
             print(count)
             break
+            
     count += 1

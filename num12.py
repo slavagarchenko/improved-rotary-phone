37218ba9dd8b69638c67156e4c09404ea2917e39
slavagarchenko <cvgarchenko@gmail.com>
2025-09-16 09:46:15 +0700

    Update num12.py

--- num12.py
+++ num12.py
@@ -1,5 +1,6 @@
 import keyword
 text = input()
+
 # метод строк который проверяет первый символ - буква, подчеркивание, и отсальные - буквы, цифры, подчеркивании
 # функция которая выдает буллинговоые значения (True/False)
 if text.isidentifier() and not keyword.iskeyword(text):

91ff9fb1f11b1d65438f457cc04f571458f10d57
slavagarchenko <cvgarchenko@gmail.com>
2025-09-16 09:39:51 +0700

    Create num9.py

---
+++ num9.py
@@ -0,0 +1,12 @@
+text = input()
+k = set()
+words = text.split()
+clean_words = []
+for word in words:
+    cleaned = word.rstrip('.,!?;:')  # удаление символа на конце слова
+    clean_words.append(cleaned)
+for i in clean_words:
+    if i in k:
+        print(i)
+    else:
+        k.add(i)

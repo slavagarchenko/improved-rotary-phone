d1f8a587dd1f0cb8a6979a42f52ad7705d061e5a
slavagarchenko <cvgarchenko@gmail.com>
2025-09-16 09:49:12 +0700

    Update num7.py

--- num7.py
+++ num7.py
@@ -1,10 +1,13 @@
 text = input()
 count = float('+inf')
 k = 0
+
 for i in range(len(text)):
     if text[i] != ' ':
         k += 1
+        
     else:
         count = min(count, k)
         k = 0
+        
 print(count)
